e63b401fdaa993fd82e47779fd5c36bd5f8e3f49
beluga-digital <d.baumert96@gmail.com>
2025-03-19 23:44:00 +0100

    fix text component widths and text colors

--- packages/ui/src/components/ui/paragraph.tsx
+++ packages/ui/src/components/ui/paragraph.tsx
@@ -4,48 +4,51 @@ import { cva } from 'class-variance-authority';
 import * as React from 'react';
 import { cn } from '@beluga/utils';
 
-const paragraphVariants = cva('text-primary leading-tight', {
-    variants: {
-        size: {
-            '3xs': 'text-2xs',
-            '2xs': 'text-xs',
-            'xs': 'text-sm',
-            'sm': 'text-base',
-            'base': 'text-base md:lg',
-            'lg': 'text-lg md:text-xl',
-            'xl': 'text-lg md:text-xl xl:text-2xl',
-            '2xl': 'text-xl md:text-2xl xl:text-3xl',
-            '3xl': 'text-2xl md:text-3xl xl:text-4xl',
-            '4xl': 'text-3xl md:text-4xl xl:text-5xl',
-            '5xl': 'text-4xl md:text-5xl xl:text-6xl',
-            '6xl': 'text-5xl md:text-6xl xl:text-7xl'
+const paragraphVariants = cva(
+    'w-full text-zinc-900 dark:text-zinc-50 leading-tight',
+    {
+        variants: {
+            size: {
+                '3xs': 'text-2xs',
+                '2xs': 'text-xs',
+                'xs': 'text-sm',
+                'sm': 'text-base',
+                'base': 'text-base md:lg',
+                'lg': 'text-lg md:text-xl',
+                'xl': 'text-lg md:text-xl xl:text-2xl',
+                '2xl': 'text-xl md:text-2xl xl:text-3xl',
+                '3xl': 'text-2xl md:text-3xl xl:text-4xl',
+                '4xl': 'text-3xl md:text-4xl xl:text-5xl',
+                '5xl': 'text-4xl md:text-5xl xl:text-6xl',
+                '6xl': 'text-5xl md:text-6xl xl:text-7xl'
+            },
+            align: {
+                left: 'text-left',
+                center: 'text-center',
+                right: 'text-right',
+                justify: 'text-justify'
+            },
+            weight: {
+                light: 'font-light',
+                normal: 'font-normal',
+                medium: 'font-medium',
+                semibold: 'font-semibold',
+                bold: 'font-bold'
+            },
+            font: {
+                sans: 'font-sans',
+                serif: 'font-serif',
+                mono: 'font-mono'
+            }
         },
-        align: {
-            left: 'text-left',
-            center: 'text-center',
-            right: 'text-right',
-            justify: 'text-justify'
-        },
-        weight: {
-            light: 'font-light',
-            normal: 'font-normal',
-            medium: 'font-medium',
-            semibold: 'font-semibold',
-            bold: 'font-bold'
-        },
-        font: {
-            sans: 'font-sans',
-            serif: 'font-serif',
-            mono: 'font-mono'
+        defaultVariants: {
+            size: 'lg',
+            align: 'left',
+            weight: 'normal',
+            font: 'sans'
         }
-    },
-    defaultVariants: {
-        size: 'lg',
-        align: 'left',
-        weight: 'normal',
-        font: 'sans'
     }
-});
+);
 
 export interface ParagraphProps
     extends React.HTMLAttributes<HTMLParagraphElement>,
