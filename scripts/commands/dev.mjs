5a026945a5a259c69479f2caf8a83626e10141ad
beluga-digital <d.baumert96@gmail.com>
2025-03-19 09:38:11 +0100

    update dev command

--- scripts/commands/dev.mjs
+++ scripts/commands/dev.mjs
@@ -5,7 +5,26 @@ import chalk from 'chalk';
 
 export function dev() {
     console.log(chalk.blue('🚀 Starting development mode...'));
+
     try {
+        const services = execSync(
+            'docker compose -f docker-compose.yaml config --services',
+            { encoding: 'utf-8' }
+        )
+            .split('\n')
+            .map((service) => service.trim())
+            .filter((service) => service.endsWith('-dev'))
+            .join(' ');
+
+        if (services) {
+            execSync(
+                `docker compose -f docker-compose.yaml up ${services} -d`,
+                { stdio: 'inherit' }
+            );
+        } else {
+            console.log(chalk.yellow('⚠️ No -dev services found to start.'));
+        }
+
         execSync('turbo run dev', { stdio: 'inherit' });
         console.log(chalk.green('✅ Development mode started!'));
     } catch (error) {
