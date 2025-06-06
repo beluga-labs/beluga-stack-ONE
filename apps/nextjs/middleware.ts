920ea7f509564483bd2c32349717126789e0b1f3
beluga-digital <d.baumert96@gmail.com>
2025-06-06 09:39:47 +0200

    move app-templates from templates in apps folder

---
+++ apps/nextjs/middleware.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+
+const locales = ['de', 'en'] as const;
+const defaultLocale = 'de';
+
+function getLocale(request: NextRequest) {
+    const negotiatorHeaders: Record<string, string> = {};
+    request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+    const languages = new Negotiator({
+        headers: negotiatorHeaders
+    }).languages();
+    return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+    const pathname = request.nextUrl.pathname;
+
+    // Check if pathname already has locale
+    const pathnameHasLocale = locales.some(
+        (locale) =>
+            pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    );
+
+    if (pathnameHasLocale) return NextResponse.next();
+
+    // Redirect if there is no locale
+    const locale = getLocale(request);
+    request.nextUrl.pathname = `/${locale}${pathname}`;
+    return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - public folder
+         */
+        '/((?!_next/static|_next/image|favicon.ico|public/|api).*)'
+    ]
+};
