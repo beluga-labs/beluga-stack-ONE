028e80029c055edb228897903a718a0de663e71a
beluga-digital <d.baumert96@gmail.com>
2025-07-09 14:34:23 +0200

    update and refactor env variables

--- packages/utils/src/env/config.ts
+++ packages/utils/src/env/config.ts
@@ -1,5 +1,22 @@
-export const getPublicUrl = (): string =>
-    process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000';
+export const getAppUrl = (): string =>
+    process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000';
+
 export const getDatabaseUrl = (): string => process.env.DATABASE_URL ?? '';
+
+export const getGitHubClientId = (): string =>
+    process.env.GITHUB_CLIENT_ID ?? '';
+export const getGitHubClientSecret = (): string =>
+    process.env.GITHUB_CLIENT_SECRET ?? '';
+
+export const getSmtpHost = (): string => process.env.SMTP_HOST ?? '';
+export const getSmtpPort = (): string => process.env.SMTP_PORT ?? '';
+export const getSmtpUser = (): string => process.env.SMTP_USER ?? '';
+export const getSmtpPassword = (): string => process.env.SMTP_PASSWORD ?? '';
+
+export const getMailSenderAddress = (): string =>
+    process.env.MAIL_SENDER_ADDRESS ?? '';
+export const getMailSenderName = (): string =>
+    process.env.MAIL_SENDER_NAME ?? '';
+
 export const isProduction = (): boolean =>
     process.env.NODE_ENV === 'production';
