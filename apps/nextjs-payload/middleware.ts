f8c7d9adc28b7b13361a78c898c6a07ac73de297
beluga-digital <d.baumert96@gmail.com>
2025-09-01 19:22:55 +0200

    add new middleware for both apps

--- apps/nextjs-payload/middleware.ts
+++ apps/nextjs-payload/middleware.ts
@@ -1,49 +1,21 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { match } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
-import { LOCALES, DEFAULT_LOCALE } from '@beluga/translations';
-import { Locale } from '@beluga/translations/types';
+import { domainRouter } from '@/lib/middleware/domainRouter';
+import { i18nRouter } from '@/lib/middleware/i18nRouter';
+import { NextRequest } from 'next/server';
 
-const locales = LOCALES;
-const defaultLocale = DEFAULT_LOCALE;
+export async function middleware(request: NextRequest) {
+    const i18nRedirectResponse = i18nRouter(request);
 
-function getLocale(request: NextRequest) {
-    const negotiatorHeaders: Record<string, string> = {};
-    request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+    if (i18nRedirectResponse) {
+        return i18nRedirectResponse;
+    }
 
-    const languages = new Negotiator({
-        headers: negotiatorHeaders
-    }).languages();
-    return match(languages, locales, defaultLocale);
-}
+    const domainRewriteResponse = domainRouter(request);
 
-export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-
-    // Check if pathname already has locale
-    const pathnameHasLocale = locales.some(
-        (locale: Locale) =>
-            pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-    );
-
-    if (pathnameHasLocale) return NextResponse.next();
-
-    // Redirect if there is no locale
-    const locale = getLocale(request);
-    request.nextUrl.pathname = `/${locale}${pathname}`;
-    return NextResponse.redirect(request.nextUrl);
+    return domainRewriteResponse;
 }
 
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * - public folder
-         */
-        '/((?!_next/static|_next/image|api/|admin/|assets/|public/|images/|videos/|favicon.ico|icon.png|icon.svg|apple-icon.png|manifest.json|sw.js).*)'
+        '/((?!api|_next/static|_next/image|admin|assets|public|images|videos|favicon.ico|icon.png|icon.svg|apple-icon.png|manifest.json|sw.js).*)'
     ]
 };
