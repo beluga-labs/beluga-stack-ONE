e8c97881cb90d2b82287c529d94c4ee03738a4b0
beluga-digital <d.baumert96@gmail.com>
2025-09-01 19:19:07 +0200

    add Payload Locale types to translation package

--- packages/translations/src/types/Locale.ts
+++ packages/translations/src/types/Locale.ts
@@ -1,3 +1,9 @@
 import { LANGUAGES } from '../languages';
 
+export type Language = (typeof LANGUAGES)[number];
 export type Locale = (typeof LANGUAGES)[number]['code'];
+export type PayloadLocale =
+    | (typeof LANGUAGES)[number]['code']
+    | 'all'
+    | 'id'
+    | undefined;
